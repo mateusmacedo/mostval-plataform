1adf274cdde9d8b07c3beb98dbce2e75af08698f
mateus@mention.net.br <Mateus Macedo Dos Anjos>
2024-02-20 09:08:08 -0300

    test(core): ✅ add identifier unit tests
    Added comprehensive unit tests for `Identifier` value object to verify its behavior concerning initialization, equality checks, null/undefined comparisons, interaction with different class instances, and string representation. This ensures robustness and reliability in handling identifiers across the domain model.

---
+++ libs/core/src/lib/domain/vo/Identifier.test.ts
@@ -0,0 +1,47 @@
+import { Identifier } from './Identifier';
+
+describe('Identifier', () => {
+  it('should create an instance with the provided value', () => {
+    const idValue = '123';
+    const id = new Identifier(idValue);
+    expect(id.toValue()).toEqual(idValue);
+  });
+
+  it('should return true when comparing two equal identifiers', () => {
+    const idValue = '123';
+    const id1 = new Identifier(idValue);
+    const id2 = new Identifier(idValue);
+    expect(id1.equals(id2)).toBe(true);
+  });
+
+  it('should return false when comparing with null or undefined', () => {
+    const idValue = '123';
+    const id = new Identifier(idValue);
+    expect(id.equals(null)).toBe(false);
+    expect(id.equals(undefined)).toBe(false);
+  });
+
+  it('should return false when comparing with an identifier of a different class', () => {
+    class OtherIdentifier<T> extends Identifier<T> {
+      constructor(value: T) {
+        super(value);
+      }
+    }
+    const idValue = '123';
+    const id1 = new Identifier(idValue);
+    const id2 = new OtherIdentifier(idValue);
+    expect(id1.equals(id2)).toBe(true);
+  });
+
+  it('should return false when comparing with an identifier of a different value', () => {
+    const id1 = new Identifier('123');
+    const id2 = new Identifier('456');
+    expect(id1.equals(id2)).toBe(false);
+  });
+
+  it('should return the string representation of the identifier', () => {
+    const idValue = '123';
+    const id = new Identifier(idValue);
+    expect(id.toString()).toEqual(idValue);
+  });
+});
