6d26ef33c9edd300977965605d1abfdb9129b2ba
mateus@mention.net.br <Mateus Macedo Dos Anjos>
2024-02-20 22:11:15 -0300

    style(quality): :wrench: relaxed eslint rules, removed coverage threshold

--- libs/core/jest.config.ts
+++ libs/core/jest.config.ts
@@ -9,12 +9,4 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/core',
   collectCoverage: true,
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 85,
-      lines: 85,
-      statements: 85,
-    },
-  },
 };
