501ff17818beaed7c335486b71cb6908cbbaa960
Mateus Macedo Dos Anjos <mateus@mention.net.br>
2024-03-11 23:10:05 -0300

    refactor(core): :hammer: update baseentity to enhance domain model flexibility and capture timestamps

--- libs/core/src/lib/domain/model/Entity.ts
+++ libs/core/src/lib/domain/model/Entity.ts
@@ -1,21 +1,44 @@
-export abstract class BaseEntity<ID> {
-  protected readonly _id: ID;
-  private _version: number;
+export type BaseEntityProps<ID> = {
+  id: ID;
+  version: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
 
-  constructor(id: ID, version = 0) {
-    this._id = id;
-    this._version = version;
+export abstract class BaseEntity<T extends BaseEntityProps<ID>, ID> {
+  protected _props: T;
+
+  constructor(props: T) {
+    this._props = props;
   }
 
-  get id(): ID {
-    return this._id;
+  public get id(): ID {
+    return this.props.id;
   }
 
-  get version(): number {
-    return this._version;
+  public get version(): number {
+    return this.props.version;
   }
 
-  incrementVersion(): void {
-    this._version += 1;
+  public incrementVersion(): void {
+    this.props.version++;
+    this.props.updatedAt = new Date();
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+
+  public get deletedAt(): Date | null {
+    return this.props.deletedAt;
+  }
+
+  public get props(): T {
+    return this._props;
   }
 }
