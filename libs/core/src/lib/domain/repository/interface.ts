280ab68ed1923af058c163515d51f86a339fe3f8
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-11-01 22:18:14 -0300

    refactor(repository): :hammer: update base entity inheritance
    Streamlined repository interfaces by standardizing the inheritance from `BaseEntity`, eliminating redundant entity definitions, and enhancing type safety across repository methods.

--- libs/core/src/lib/domain/repository/interface.ts
+++ libs/core/src/lib/domain/repository/interface.ts
@@ -1,19 +1,16 @@
 import { Result } from '../../application';
 import { DependencyError, NotFoundError, ValidationError } from '../error';
+import { BaseEntity } from '../model';
 
-export interface Entity<ID> {
-  id: ID;
-}
-
-export interface IFindById<T extends Entity<ID>, ID> {
+export interface IFindById<T extends BaseEntity<T, ID>, ID> {
   findById(id: ID): Promise<Result<T, DependencyError | NotFoundError>>;
 }
 
-export interface IFindAll<T extends Entity<ID>, ID> {
+export interface IFindAll<T extends BaseEntity<T, ID>, ID> {
   findAll(): Promise<Result<T[], DependencyError>>;
 }
 
-export interface ISave<T extends Entity<ID>, ID> {
+export interface ISave<T extends BaseEntity<T, ID>, ID> {
   save(entity: T): Promise<Result<T, DependencyError | ValidationError>>;
 }
 
@@ -21,7 +18,7 @@ export interface IDelete<ID> {
   delete(id: ID): Promise<Result<void, DependencyError | NotFoundError>>;
 }
 
-export interface IRepository<T extends Entity<ID>, ID, E = string>
+export interface IRepository<T extends BaseEntity<T, ID>, ID>
   extends IFindById<T, ID>,
     IFindAll<T, ID>,
     ISave<T, ID>,
