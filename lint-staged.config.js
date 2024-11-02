3d611c1a23bf5948d53490d445d0f0a12e7baa99
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-11-01 22:15:11 -0300

    chore(lint): :wrench: remove redundant lint fix step
    Eliminated the redundant `lint --fix` step from the lint-staged configuration to enhance efficiency. Updated `tsconfig` to use ES2022 for improved compatibility and new language features.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   '{libs,apps}/**/*.{js,ts,tsx}': [
     'nx affected --target test --uncommitted',
-    'nx affected --target lint --uncommitted --fix=true',
     'nx format:write --uncommitted',
   ],
 };
