f250b65959722dab0b12b30f731b25027cfc5eec
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-11-02 00:23:59 -0300

    feat(logger): :sparkles: introduce pino transport for logging
    Added a new PinoTransport implementation to enhance the logging infrastructure with flexible log level configuration, transport options, and destination handling, leveraging the `pino` library's capabilities. This enables more robust logging management and integration.

---
+++ libs/core/src/lib/infrastructure/logger/transport/pino.ts
@@ -0,0 +1,41 @@
+import pino from 'pino';
+import { LogLevel, LogTransport } from '../../../application/Logger';
+
+export interface PinoTransportOptions {
+  level?: LogLevel;
+  transport?: pino.TransportSingleOptions;
+  destination?: pino.DestinationStream | undefined;
+}
+
+export class PinoTransport implements LogTransport {
+  private pinoLogger: pino.Logger;
+
+  constructor(options: PinoTransportOptions = {}) {
+    const { level = LogLevel.INFO, transport, destination } = options;
+    const logLevel = this.mapLogLevel(level);
+    this.pinoLogger = pino(
+      {
+        level: logLevel,
+        transport,
+      },
+      destination,
+    );
+  }
+
+  private mapLogLevel(level: LogLevel): pino.LevelWithSilent {
+    const levelMapping: Record<LogLevel, pino.LevelWithSilent> = {
+      [LogLevel.FATAL]: 'fatal',
+      [LogLevel.ERROR]: 'error',
+      [LogLevel.WARN]: 'warn',
+      [LogLevel.INFO]: 'info',
+      [LogLevel.DEBUG]: 'debug',
+      [LogLevel.TRACE]: 'trace',
+    };
+    return levelMapping[level] || 'info';
+  }
+
+  log(level: LogLevel, message: string, meta?: unknown): void {
+    const logLevel = this.mapLogLevel(level);
+    this.pinoLogger[logLevel](meta, message);
+  }
+}
