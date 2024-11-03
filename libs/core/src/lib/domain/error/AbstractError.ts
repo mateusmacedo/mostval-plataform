570670265e1628436405906e94cd9ea3aad64471
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-11-03 01:00:13 -0300

    refactor(error): :hammer: improve error handling defaults
    Set default values for 'code' and 'previousError' to enhance error object initialization and ensure safer handling of optional parameters. This change provides a clearer contract for error handling, reducing potential null-check overhead.

--- libs/core/src/lib/domain/error/AbstractError.ts
+++ libs/core/src/lib/domain/error/AbstractError.ts
@@ -1,19 +1,19 @@
 export abstract class AbstractError<TError> {
   constructor(
     private errorOrMessage: TError,
-    private code?: number,
-    private previousError?: AbstractError<any>,
+    private code: number = 0,
+    private previousError: AbstractError<any> | null = null,
   ) {}
 
   getError(): TError {
     return this.errorOrMessage;
   }
 
-  getCode(): number {
+  getCode(): number | undefined {
     return this.code;
   }
 
-  getPreviousError(): AbstractError<any> {
+  getPreviousError(): AbstractError<any> | null {
     return this.previousError;
   }
 }
