ce9063027b1cfe1e08a312ee632b3f8515d796b1
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2024-11-03 01:02:09 -0300

    refactor(http): :hammer: support optional headers and interceptors
    Refactor `AxiosHttpClient` to allow optional headers for requests and include support for interceptors.
    Adjusted result handling with a new `Result` type, enhancing error management.

--- libs/core/src/lib/infrastructure/http/client/AxiosHttpClient.ts
+++ libs/core/src/lib/infrastructure/http/client/AxiosHttpClient.ts
@@ -1,59 +1,91 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import {
-  HttpClientInterface,
-  HttpClientResponseProps,
-  httpClientProps,
-} from './HttpClientInterface';
+
+import { HttpClientInterface, HttpClientProps, Result } from './HttpClientInterface';
 
 export class AxiosHttpClient implements HttpClientInterface {
-  private clientConfig!: AxiosRequestConfig;
+  private clientConfig: AxiosRequestConfig;
 
-  constructor(private config: httpClientProps = {}) {
-    this.makeInstanceConfig(this.config);
-  }
-
-  private makeInstanceConfig(config: httpClientProps): void {
+  constructor(private config: HttpClientProps = {}) {
     this.clientConfig = {
       baseURL: config.baseUrl,
       headers: config.headers,
       responseType: config.responseType ?? 'json',
       timeout: config.requestTimeout,
     };
+
+    // Configuração dos interceptors, se fornecidos
+    if (config.requestInterceptor) {
+      axios.interceptors.request.use(config.requestInterceptor);
+    }
+    if (config.responseInterceptor) {
+      axios.interceptors.response.use(config.responseInterceptor);
+    }
   }
 
   private async request<TResponse>(
     method: string,
     url: string,
-    data?: object,
-  ): Promise<HttpClientResponseProps<TResponse>> {
+    data?: any,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>> {
     try {
-      const response = await axios.request({ ...this.clientConfig, method, url, data });
+      const response = await axios.request<TResponse>({
+        ...this.clientConfig,
+        method,
+        url,
+        data,
+        headers: { ...this.clientConfig.headers, ...headers },
+      });
+
       return {
-        status: response.status,
-        data: response.data as TResponse,
-      } as HttpClientResponseProps<TResponse>;
+        success: true,
+        data: {
+          status: response.status,
+          data: response.data,
+        },
+      };
     } catch (error: any) {
-      throw new Error(
-        JSON.stringify({
-          status: error?.response?.status ?? 500,
-          data: error?.response?.data ?? 'Internal Error',
-        }),
-      );
+      return {
+        success: false,
+        error: new Error(
+          JSON.stringify({
+            status: error?.response?.status ?? 500,
+            data: error?.response?.data ?? 'Erro Interno',
+          }),
+        ),
+      };
     }
   }
-  get<TResponse>(uri: string): Promise<HttpClientResponseProps<TResponse>> {
-    return this.request('get', uri);
+
+  get<TResponse>(uri: string, headers?: Record<string, string>): Promise<Result<TResponse>> {
+    return this.request<TResponse>('get', uri, undefined, headers);
   }
-  post<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>> {
-    return this.request('post', uri, data);
+
+  post<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>> {
+    return this.request<TResponse>('post', uri, data, headers);
   }
-  put<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>> {
-    return this.request('put', uri, data);
+
+  put<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>> {
+    return this.request<TResponse>('put', uri, data, headers);
   }
-  delete<TResponse>(uri: string): Promise<HttpClientResponseProps<TResponse>> {
-    return this.request('delete', uri);
+
+  delete<TResponse>(uri: string, headers?: Record<string, string>): Promise<Result<TResponse>> {
+    return this.request<TResponse>('delete', uri, undefined, headers);
   }
-  patch<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>> {
-    return this.request('patch', uri, data);
+
+  patch<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>> {
+    return this.request<TResponse>('patch', uri, data, headers);
   }
 }
--- libs/core/src/lib/infrastructure/http/client/HttpClientInterface.ts
+++ libs/core/src/lib/infrastructure/http/client/HttpClientInterface.ts
@@ -1,19 +1,41 @@
 export type HttpClientResponseProps<TResponse> = {
   status: number | string;
-  data?: TResponse | undefined;
+  data?: TResponse;
 };
-export type httpClientProps = {
+
+export type HttpClientProps = {
   baseUrl?: string;
-  headers?: { [key: string]: string };
+  headers?: Record<string, string>;
   responseType?: 'arraybuffer' | 'document' | 'json' | 'text' | 'stream';
   requestTimeout?: number;
   requestInterceptor?: (config: any) => Promise<any>;
   responseInterceptor?: (response: any) => Promise<any>;
 };
+
+export type Result<TResponse, TError = Error> =
+  | { success: true; data: HttpClientResponseProps<TResponse> }
+  | { success: false; error: TError };
+
 export interface HttpClientInterface {
-  get<TResponse>(uri: string): Promise<HttpClientResponseProps<TResponse>>;
-  post<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>>;
-  put<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>>;
-  delete<TResponse>(uri: string): Promise<HttpClientResponseProps<TResponse>>;
-  patch<TResponse>(uri: string, data: object): Promise<HttpClientResponseProps<TResponse>>;
+  get<TResponse>(uri: string, headers?: Record<string, string>): Promise<Result<TResponse>>;
+
+  post<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>>;
+
+  put<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>>;
+
+  delete<TResponse>(uri: string, headers?: Record<string, string>): Promise<Result<TResponse>>;
+
+  patch<TRequest, TResponse>(
+    uri: string,
+    data: TRequest,
+    headers?: Record<string, string>,
+  ): Promise<Result<TResponse>>;
 }
