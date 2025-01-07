fed3126ae1f8ef2ad085b7f6cebc912275b12592
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-06 23:50:47 -0300

    test(identifier): 🧪 update Identifier tests for improved clarity and consistency
    Refactored tests for the Identifier class to enhance readability and maintainability. Changes include updating method calls from `toValue()` to `getValue()`, ensuring consistent use of TypeScript's type system, and improving the structure of test cases. This update aims to provide clearer assertions and better alignment with the latest implementation of the Identifier class.

--- libs/core/src/lib/domain/vo/Identifier.test.ts
+++ libs/core/src/lib/domain/vo/Identifier.test.ts
@@ -1,40 +1,41 @@
-import { Identifier } from './Identifier';
+import { Identifier } from './Identifier'
+import { Primitives } from './ValueObject'
 
 describe('Identifier', () => {
   it('should create an instance with the provided value', () => {
-    const idValue = '123';
-    const id = new Identifier(idValue);
-    expect(id.toValue()).toEqual(idValue);
-  });
+    const idValue = '123'
+    const id = new Identifier(idValue)
+    expect(id.getValue()).toEqual(idValue)
+  })
 
   it('should return true when comparing two equal identifiers', () => {
-    const idValue = '123';
-    const id1 = new Identifier(idValue);
-    const id2 = new Identifier(idValue);
-    expect(id1.equals(id2)).toBe(true);
-  });
+    const idValue = '123'
+    const id1 = new Identifier(idValue)
+    const id2 = new Identifier(idValue)
+    expect(id1.equals(id2)).toBe(true)
+  })
 
   it('should return false when comparing with an identifier of a different class', () => {
-    class OtherIdentifier<T> extends Identifier<T> {
+    class OtherIdentifier<T extends Primitives | Primitives[]> extends Identifier<T> {
       constructor(value: T) {
-        super(value);
+        super(value)
       }
     }
-    const idValue = '123';
-    const id1 = new Identifier(idValue);
-    const id2 = new OtherIdentifier(idValue);
-    expect(id1.equals(id2)).toBe(false);
-  });
+    const idValue = '123' as const
+    const id1 = new Identifier(idValue)
+    const id2 = new OtherIdentifier(idValue)
+    expect(id1.equals(id2)).toBe(false)
+  })
 
   it('should return false when comparing with an identifier of a different value', () => {
-    const id1 = new Identifier('123');
-    const id2 = new Identifier('456');
-    expect(id1.equals(id2)).toBe(false);
-  });
+    const id1 = new Identifier<string>('123')
+    const id2 = new Identifier<string>('456')
+    expect(id1.equals(id2)).toBe(false)
+  })
 
   it('should return the string representation of the identifier', () => {
-    const idValue = '123';
-    const id = new Identifier(idValue);
-    expect(id.toString()).toEqual(idValue);
-  });
-});
+    const idValue = '123'
+    const id = new Identifier(idValue)
+    expect(id.toString()).toEqual(idValue)
+  })
+})
