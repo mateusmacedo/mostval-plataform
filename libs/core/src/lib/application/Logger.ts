c32543810382da6c79b6abb13cc916a63acd0efd
Mateus Macedo Dos Anjos <macedodosanjosmateus@gmail.com>
2025-01-07 00:08:02 -0300

    refactor(logging): 🛠️ enhance Logger and Result classes with improved structure and functionality
    This commit introduces several enhancements to the Logger and Result classes. The Logger interface has been updated to include a new LogHandler function, which facilitates logging during message handling, improving debugging capabilities. Additionally, the Result class has been refactored to streamline its methods, enhance type safety, and introduce new utility methods such as map and mapError for better handling of success and failure cases. These changes aim to improve code clarity, maintainability, and overall functionality within the application.

--- libs/core/src/lib/application/Logger.ts
+++ libs/core/src/lib/application/Logger.ts
@@ -1,3 +1,6 @@
+import { IHandler } from '../domain/model/Handlers'
+import { Message } from '../domain/model/Message'
+
 export enum LogLevel {
   FATAL = 'fatal',
   ERROR = 'error',
@@ -8,33 +11,59 @@ export enum LogLevel {
 }
 
 export interface Logger {
-  log(level: LogLevel, message: string, meta?: unknown): void;
-  info(message: string, meta?: unknown): void;
-  error(message: string, meta?: unknown): void;
-  warn(message: string, meta?: unknown): void;
-  debug(message: string, meta?: unknown): void;
-  trace(message: string, meta?: unknown): void;
-  fatal(message: string, meta?: unknown): void;
+  log(level: LogLevel, message: string, meta?: unknown): void
+  info(message: string, meta?: unknown): void
+  error(message: string, meta?: unknown): void
+  warn(message: string, meta?: unknown): void
+  debug(message: string, meta?: unknown): void
+  trace(message: string, meta?: unknown): void
+  fatal(message: string, meta?: unknown): void
 }
 
 export interface LogTransport {
-  log(level: LogLevel, message: string, meta?: unknown): void;
+  log(level: LogLevel, message: string, meta?: unknown): void
 }
 
 export interface LogFormatter {
-  format(level: LogLevel, message: string, meta?: unknown): string;
+  format(level: LogLevel, message: string, meta?: unknown): string
 }
 
 export interface LoggerWithTransport extends Logger {
-  addTransport(transport: LogTransport): void;
-  removeTransport(transport: LogTransport): void;
-  setLogLevel(level: LogLevel): void;
-  setDefaultMeta(meta: ILogContext): void;
+  addTransport(transport: LogTransport): void
+  removeTransport(transport: LogTransport): void
+  setLogLevel(level: LogLevel): void
+  setDefaultMeta(meta: ILogContext): void
 }
 
 export interface ILogContext {
-  requestId?: string;
-  userId?: string;
-  transactionId?: string;
-  [key: string]: unknown;
+  requestId?: string
+  userId?: string
+  transactionId?: string
+  [key: string]: unknown
+}
+
+export function LogHandler(logger?: Logger) {
+  return function <T extends Message<any>, R, E extends Error>(
+    target: IHandler<T, R, E>,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value
+    const log = logger ?? console
+
+    descriptor.value = async function (...args: any[]) {
+      const message = args[0]
+      log.info(`Handling ${message.type}`, { timestamp: new Date().toISOString() })
+
+      try {
+        const result = await originalMethod.apply(this, args)
+        log.info(`Handled ${message.type} successfully`)
+        return result
+      } catch (error) {
+        log.error(`Error handling ${message.type}`, { error })
+        throw error
+      }
+    }
+    return descriptor
+  }
 }
--- libs/core/src/lib/application/Result.ts
+++ libs/core/src/lib/application/Result.ts
@@ -4,76 +4,101 @@ export class Result<T, E = Error> {
     private readonly error?: E,
     private readonly value?: T,
   ) {
-    Object.freeze(this);
+    Object.freeze(this)
   }
 
   public get isSuccess(): boolean {
-    return this.success;
+    return this.success
   }
 
   public get isFailure(): boolean {
-    return !this.success;
+    return !this.success
   }
 
   public getValue(): T {
     if (this.isFailure) {
-      throw new Error('Cannot get the value of a failed result.');
+      throw new Error('Cannot get the value of a failed result.')
     }
-    return this.value as T;
+    return this.value as T
   }
 
   public getError(): E {
     if (this.isSuccess) {
-      throw new Error('Cannot get the error of a successful result.');
+      throw new Error('Cannot get the error of a successful result.')
     }
-    return this.error as E;
+    return this.error as E
   }
 
   public static success<U, E = Error>(value?: U): Result<U, E> {
-    return new Result<U, E>(true, undefined, value);
+    return new Result<U, E>(true, undefined, value)
   }
 
   public static failure<U = never, E = unknown>(error: E): Result<U, E> {
     if (error === undefined) {
-      throw new Error('Error cannot be undefined');
+      throw new Error('Error cannot be undefined')
     }
-    return new Result<U, E>(false, error);
+    return new Result<U, E>(false, error)
   }
 
-  public static combine(...results: Array<Result<unknown, unknown>>): Result<unknown, unknown> {
-    const errors = results.filter((result) => result.isFailure).map((result) => result.getError());
-    return errors.length > 0 ? Result.failure(errors) : Result.success();
+  public static combine<T, E>(...results: Array<Result<T, E>>): Result<T[], E> {
+    const errors = results.filter((result) => result.isFailure)
+    if (errors.length > 0) {
+      return Result.failure(errors[0].getError())
+    }
+
+    const values = results.map((result) => result.getValue())
+    return Result.success(values)
   }
 
   public async andThenAsync<U>(func: (value: T) => Promise<Result<U, E>>): Promise<Result<U, E>> {
     if (this.isFailure) {
-      return Result.failure<U, E>(this.getError());
+      return Result.failure<U, E>(this.getError())
     }
-    return await func(this.getValue());
+    return await func(this.getValue())
   }
 
   public andThen<U>(func: (value: T) => Result<U, E>): Result<U, E> {
     if (this.isFailure) {
-      return Result.failure<U, E>(this.getError());
+      return Result.failure<U, E>(this.getError())
     }
-    return func(this.getValue());
+    return func(this.getValue())
   }
 
   public onFailure(func: (error: E) => void): Result<T, E> {
     if (this.isFailure) {
-      func(this.getError());
+      func(this.getError())
     }
-    return this;
+    return this
   }
 
   public onSuccess(func: (value: T) => void): Result<T, E> {
     if (this.isSuccess) {
-      func(this.getValue());
+      func(this.getValue())
     }
-    return this;
+    return this
+  }
+
+  public map<U>(func: (value: T) => U): Result<U, E> {
+    if (this.isFailure) {
+      return Result.failure(this.getError())
+    }
+    return Result.success(func(this.getValue()))
+  }
+
+  public mapError<F>(func: (error: E) => F): Result<T, F> {
+    if (this.isSuccess) {
+      return Result.success(this.getValue())
+    }
+    return Result.failure(func(this.getError()))
+  }
+
+  public static fromPromise<T, E = Error>(promise: Promise<T>): Promise<Result<T, E>> {
+    return promise
+      .then((value) => Result.success<T, E>(value))
+      .catch((error) => Result.failure<T, E>(error))
   }
 }
 
-export type ResultOk<T> = Result<T, never>;
-export type ResultFail<E> = Result<never, E>;
-export type ResultOr<T, E> = Result<T, E>;
+export type ResultOk<T> = Result<T, never>
+export type ResultFail<E> = Result<never, E>
+export type ResultOr<T, E> = Result<T, E>
